6774aed477958c2399168596ab9adcd84fe1c3fe
Maxime Tan <maxime.tan1@gmail.com>
2023-09-06 10:38:27 +0200

    commit
    arrow click + essai currentDot

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,25 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let currentDot = document.querySelector(".dot");
+let currentImage = document.getElementById("2");
+
+	if (currentImage = currentDot) {
+		currentDot.classList.add('dot_selected')
+	};
+
+
+
+
+// un peu de funz
+	const arrowClick = document.querySelector('.arrow_right');
+	arrowClick.addEventListener("click" , ()=>  {
+		const body = document.querySelector("body")
+		body.style.backgroundColor="blue"
+	})
+	const arrowClick2 = document.querySelector('.arrow_left');
+	arrowClick2.addEventListener("click" , ()=>  {
+		const body = document.querySelector("body")
+		body.style.backgroundColor="red"
+	})	
