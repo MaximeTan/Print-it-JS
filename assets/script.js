038d7d8212992ae79e19163494a0d62449ea9de8
Maxime Tan <maxime.tan1@gmail.com>
2023-09-07 22:21:28 +0200

    commit
    - Dots
    
    - Faire les images / texte

--- assets/script.js
+++ assets/script.js
@@ -17,24 +17,59 @@ const slides = [
 	}
 ]
 
-let currentDot = document.querySelector(".dot");
-let currentImage = document.getElementById("2");
-
-	if (currentImage = currentDot) {
-		currentDot.classList.add('dot_selected')
-	};
+// let bannerSlider = document.querySelector("")
 
 
+// let currentDot = document.querySelector(".dot");
+// let currentImage = document.getElementById("2");
 
+// 	if (currentImage = currentDot) {
+// 		currentDot.classList.add('dot_selected')
+// 	};
+// const clickDroit = document.querySelector('.arrow_right');
+// clickDroit.addEventListener("click" ,(event)=>  {
+// 	const movingRight = document.getElementById("banner")
+// 	movingRight.scrollRight, nbr+=1;
+// 	console.log(event)
+// })
+// const clickGauche = document.querySelector('.arrow_left');
+// clickGauche.addEventListener("click" , ()=>  {
+// 	const movingLeft = document.getElementById("banner")
+// 	movingLeft, p-=1;
+// })
 
-// un peu de funz
-	const arrowClick = document.querySelector('.arrow_right');
-	arrowClick.addEventListener("click" , ()=>  {
-		const body = document.querySelector("body")
-		body.style.backgroundColor="blue"
-	})
-	const arrowClick2 = document.querySelector('.arrow_left');
-	arrowClick2.addEventListener("click" , ()=>  {
-		const body = document.querySelector("body")
-		body.style.backgroundColor="red"
-	})	
+	const dotSelected = document.querySelectorAll(".dot");
+	const nbSlide = dotSelected.length;
+	const next = document.querySelector(".arrow_right");
+	const previous = document.querySelector(".arrow_left");
+	let count = 0;
+
+	function nextSlide() {
+		dotSelected[count].classList.remove("dot_selected");
+			
+			if(count < nbSlide - 1) {
+				count++;
+			}
+			else {count = 0
+			};
+		dotSelected[count].classList.add("dot_selected")
+		console.log(count);
+	}
+
+	next.addEventListener("click", (nextSlide));
+
+	function previousSlide() {
+		dotSelected[count].classList.remove("dot_selected");
+			
+			if(count > 0) {
+				count--;
+			}
+			else {count = nbSlide -1;
+			}
+		dotSelected[count].classList.add("dot_selected")
+		console.log(count);
+	}
+
+	previous.addEventListener("click", (previousSlide));
+
+		
